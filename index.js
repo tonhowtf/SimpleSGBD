b46d911a31d8ea877ecda31dfed74f0eb7e46c23
Tonho <antonioreverso.com@gmail.com>
2025-05-06 21:03:52 -0300

    Add initial table creation logic and logging

---
+++ index.js
@@ -0,0 +1,5 @@
+const command = 'create table author (id number, name string, age number, city string, state string, country string)'
+const tableName = command.match(/create table (\w+)/)[1];
+const columns = command.match(/\((.+)\)/)[1];
+const columnscomma = columns.split(",").map(col => col.trim());
+
